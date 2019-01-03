8198f47b5c1bf3ff02f2a4335b2fa0d181dc487e
onlyhiero <shawn.descamps@gmail.com>
2019-01-03 19:17:58 +0100

    rajout du fichier hello.js

---
+++ hello.js
@@ -0,0 +1,10 @@
+function xxx(){
+var popup = confirm("Veux tu voir la meilleure video de cul de l'histoire ?");
+if (popup==true) {
+	location = ("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
+else { alert("Tant pis pour toi")
+
+}
+}
+setTimeout(xxx, 2000);
