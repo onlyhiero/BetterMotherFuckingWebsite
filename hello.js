8126a72492df75eaacf4065f1b6019671057d6f0
onlyhiero <shawn.descamps@gmail.com>
2019-01-03 19:21:48 +0100

    modification de la durée du timeout

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 function xxx(){
-var popup = confirm("Veux tu voir la meilleure video de cul de l'histoire ?");
+var popup = confirm("Veux tu voir la meilleure video de cul de l'histoire sur xxxvidsxxx ?");
 if (popup==true) {
 	location = ("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 }
@@ -7,4 +7,4 @@ else { alert("Tant pis pour toi")
 
 }
 }
-setTimeout(xxx, 2000);
+setTimeout(xxx, 10000);
